de91b8d8f361ffc2a358d33582cf90a1ea026bed
wynn <yowangyuxi@outlook.com>
2022-05-26 13:12:47 +0800

    IL Invoker (manage monohehaviour invokes)

--- Assets/ILShell/Runtime/ILAPP.cs
+++ Assets/ILShell/Runtime/ILAPP.cs
@@ -1,7 +1,13 @@
-﻿using ILRuntime.Runtime.Enviorment;
+﻿using ILRuntime.CLR.TypeSystem;
+using ILRuntime.Runtime.Enviorment;
+using ILShell.Adapter;
+using ILShell.Runtime.Binding;
+using System.Collections;
+using System.IO;
 using UnityEngine;
+using UnityEngine.Networking;
 
-namespace ILShell
+namespace ILShell.Runtime
 {
     public class ILAPP : MonoBehaviour
     {
@@ -11,7 +17,7 @@ namespace ILShell
 
         private static AppDomain domainForEditorMode;
 
-        public static ILAPP CurrentInstance
+        public static ILAPP Current
         {
             get
             {
@@ -19,7 +25,8 @@ namespace ILShell
                     return currentInstance;
                 else
                 {
-                    throw new System.Exception("[ILAPP]Current APP not found");
+                    currentInstance = CreateInstance("Global");
+                    return currentInstance;
                 }
             }
         }
@@ -35,25 +42,134 @@ namespace ILShell
                 }
                 else
                 {
-#if UNITY_EDITOR
-                    domainForEditorMode = domainForEditorMode ?? CreateEditorDomain();
-                    return domainForEditorMode;
-#endif
-                    throw new System.Exception("[ILAPP]Current Domain not found");
+                    if (!Application.isPlaying)
+                    {
+                        domainForEditorMode = domainForEditorMode ?? CreateEditorDomain();
+                        return domainForEditorMode;
+                    }
+                    else
+                    {
+                        currentInstance = CreateInstance("Global");
+                        return currentInstance?.AppDomain;
+                    }
                 }
             }
         }
 
+        public static ILAPP CreateInstance(string name)
+        {
+            GameObject go = new GameObject();
+            go.hideFlags = HideFlags.DontSave;
+            go.name = $"{typeof(ILAPP).Name}({name})";
+            var instance = go.AddComponent<ILAPP>();
+            instance.Name = name;
+            instance.Initialize();
+            return instance;
+        }
+
         private static AppDomain CreateEditorDomain()
         {
-            throw new System.NotImplementedException();
+#if UNITY_EDITOR
+            var AppDomain = new AppDomain();
+            var findPath = Path.Combine(Path.GetDirectoryName(Application.dataPath), "Library/ScriptAssemblies/");
+            string[] files = Directory.GetFiles(findPath, "Product.*.dll", SearchOption.TopDirectoryOnly);
+            foreach (string path in files)
+            {
+                var dllpath = path;
+                var pdbpath = Path.ChangeExtension(path, ".pdb");
+                LoadAssemblyLocal(AppDomain, dllpath, pdbpath);
+            }
+            Debug.LogWarning($"[ILAPP]CreateDefaultDomain in DEBUG!!!");
+
+            AppDomain.RegisterCrossBindingAdaptor(new MonoBehaviourAdapter());
+            CLRBindingsFixed.Initialize(AppDomain);
+
+            return AppDomain;
+#endif
+            throw new System.Exception("Debug Method");
+        }
+
+        public static IType GetType(string typeName)
+        {
+            return CurrentDomain.GetType(typeName);
         }
 
         #endregion Static
 
         #region Instance
 
+        public string Name { get; private set; }
+
         public AppDomain AppDomain { get; private set; }
+
+        public ILInvoker ILInvoker { get; private set; }
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+        private void Update()
+        {
+            ILInvoker.Update();
+        }
+
+
+        public void Initialize()
+        {
+            AppDomain = CreateEditorDomain();
+            ILInvoker = ILInvoker.CreateInstance();
+        }
+
+        public static void LoadAssemblyLocal(AppDomain domain, string dllpath, string pdbpath = null)
+        {
+            byte[] dlldata = null, pdbdata = null;
+            dlldata = File.ReadAllBytes(dllpath);
+            if (pdbpath != null)
+            {
+                pdbdata = File.ReadAllBytes(pdbpath);
+                domain.LoadAssembly(new MemoryStream(dlldata), new MemoryStream(pdbdata), new ILRuntime.Mono.Cecil.Pdb.PdbReaderProvider());
+            }
+            else
+            {
+                domain.LoadAssembly(new MemoryStream(dlldata));
+            }
+            Debug.Log($"LoadAssemblyLocalFinish: {dllpath}: {dlldata?.Length} & {pdbpath}: {pdbdata?.Length}");
+        }
+
+        public static IEnumerator LoadAssemblyRemote(AppDomain domain, string dllurl, string pdburl = null)
+        {
+            byte[] dlldata = null, pdbdata = null;
+            using (UnityWebRequest uwr = new UnityWebRequest(dllurl))
+            {
+                uwr.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+                yield return uwr.SendWebRequest();
+                if (uwr.isDone && uwr.result == UnityWebRequest.Result.Success)
+                {
+                    dlldata = uwr.downloadHandler.data;
+                }
+            }
+            if (dlldata == null)
+                throw new System.Exception($"Cannot load DLL: {dllurl}");
+            if (pdburl != null)
+            {
+                using (UnityWebRequest uwr = new UnityWebRequest(pdburl))
+                {
+                    uwr.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+                    yield return uwr.SendWebRequest();
+                    if (uwr.isDone && uwr.result == UnityWebRequest.Result.Success)
+                    {
+                        pdbdata = uwr.downloadHandler.data;
+                    }
+                }
+                domain.LoadAssembly(new MemoryStream(dlldata), new MemoryStream(pdbdata), new ILRuntime.Mono.Cecil.Pdb.PdbReaderProvider());
+            }
+            else
+            {
+                domain.LoadAssembly(new MemoryStream(dlldata));
+            }
+            Debug.Log($"LoadAssemblyRemoteFinish: {dllurl} : {pdburl}");
+        }
 
         #endregion Instance
     }
---
+++ Assets/ILShell/Runtime/ILInvoker.cs
@@ -0,0 +1,231 @@
+﻿using ILRuntime.CLR.Method;
+using ILRuntime.CLR.TypeSystem;
+using ILRuntime.Runtime.Enviorment;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ILShell.Runtime
+{
+    public class ILInvoker
+    {
+        public class InvokeItem
+        {
+            public float Time;
+            public object Instance;
+            public string MethodName;
+            public float LoopTime = -1;
+        }
+
+        public class TypeInvoker
+        {
+            private struct MethodCache
+            {
+                public string Name;
+                public IMethod Method;
+            }
+
+            private ILType Type;
+            private ILRuntime.Runtime.Enviorment.AppDomain AppDomain;
+            private Dictionary<string, MethodCache> Cache;
+
+            public TypeInvoker(ILType type)
+            {
+                if (type == null)
+                    throw new ArgumentNullException("type");
+                Type = type;
+                AppDomain = type.AppDomain;
+                Cache = new Dictionary<string, MethodCache>();
+            }
+
+            public object Invoke(CrossBindingAdaptorType agent, string methodName, params object[] args)
+            {
+                MethodCache cache;
+                IMethod method;
+                if (Cache.TryGetValue(methodName, out cache))
+                {
+                    method = cache.Method;
+                }
+                else
+                {
+                    method = Type.GetMethod(methodName, args?.Length ?? 0);
+                    cache = new MethodCache();
+                    cache.Name = methodName;
+                    cache.Method = method;
+                    Cache.Add(methodName, cache);
+                }
+                if (method == null)
+                {
+                    //Debug.LogError($"[ILInvoker]can not find method \"{methodName}\" in type \"{Type.Name}\"");
+                    return null;
+                }
+                return AppDomain.Invoke(method, agent.ILInstance, args);
+            }
+        }
+
+        private static Comparison<InvokeItem> defaultComparison = delegate (InvokeItem a, InvokeItem b)
+        {
+            if (a == b)
+                return 0;
+            else if (a.Time > b.Time)
+                return 1;
+            else if (a.Time < b.Time)
+                return -1;
+            else
+            {
+                var ha = a.GetHashCode();
+                var hb = b.GetHashCode();
+                if (ha > hb)
+                    return 1;
+                else if (ha < hb)
+                    return -1;
+                else
+                    return 0;
+            }
+        };
+
+        public static ILInvoker CreateInstance()
+        {
+            return CreateInstance(defaultComparison);
+        }
+
+        public static ILInvoker CreateInstance(Comparison<InvokeItem> comparison)
+        {
+            var ins = new ILInvoker();
+            ins.currentTime = 0;
+            if (comparison == null)
+                ins.invokeItems = new SortedSet<InvokeItem>();
+            else
+                ins.invokeItems = new SortedSet<InvokeItem>(Comparer<InvokeItem>.Create(comparison));
+            ins.lastInvoked = new List<InvokeItem>();
+            ins.typeInvokerMap = new Dictionary<ILType, TypeInvoker>();
+            ins.InvokeHandler = ins.DefaultInvokeHandler;
+            return ins;
+        }
+
+        private float currentTime;
+        private List<InvokeItem> lastInvoked;
+        private SortedSet<InvokeItem> invokeItems;
+        private Dictionary<ILType, TypeInvoker> typeInvokerMap;
+
+        public Action<object, string> InvokeHandler { private get; set; }
+
+        private void DefaultInvokeHandler(object instance, string mathodName)
+        {
+            var agent = instance as CrossBindingAdaptorType;
+            if (agent == null)
+                return;
+            GetTypeInvoker(agent).Invoke(agent, mathodName);
+        }
+
+        private ILInvoker()
+        {
+        }
+
+        public void Update()
+        {
+            var deltaTime = Time.deltaTime;
+            Update(deltaTime);
+        }
+
+        public void Update(float deltaTime)
+        {
+            lastInvoked.Clear();
+            currentTime += deltaTime;
+            foreach (var invokeItem in invokeItems)
+                if (invokeItem.Time <= currentTime)
+                    lastInvoked.Add(invokeItem);
+                else break;
+            foreach (var invokeItem in lastInvoked)
+            {
+                if (!invokeItems.Remove(invokeItem))
+                    Debug.LogError($"[ILInvoker]\"{invokeItem.MethodName}\" of {invokeItem.Instance} cannot be removed");
+                if (invokeItem.LoopTime > 0)
+                {
+                    invokeItem.Time += invokeItem.LoopTime;
+                    invokeItems.Add(invokeItem);
+                }
+                InvokeInternal(invokeItem.Instance, invokeItem.MethodName);
+            }
+        }
+
+        private void InvokeInternal(object instance, string methodName)
+        {
+            if (instance == null) return;
+            //Debug.Log($"[ILInvoker]\"{methodName}\" of {instance} is invoked");
+            if (InvokeHandler != null)
+                try
+                {
+                    InvokeHandler(instance, methodName);
+                }
+                catch (Exception ex)
+                {
+                    Debug.LogException(ex);
+                }
+            else
+                Debug.LogWarning($"[ILInvoker]InvokeHandler not found");
+        }
+
+        public void CancelInvoke(object instance)
+        {
+            lastInvoked.Clear();
+            foreach (var invokeItem in invokeItems)
+                if (invokeItem.Instance == instance)
+                    lastInvoked.Add(invokeItem);
+            foreach (var invokeItem in lastInvoked)
+                invokeItems.Remove(invokeItem);
+        }
+
+        public void CancelInvoke(object instance, string methodName)
+        {
+            lastInvoked.Clear();
+            foreach (var invokeItem in invokeItems)
+                if (invokeItem.Instance == instance && invokeItem.MethodName == methodName)
+                    lastInvoked.Add(invokeItem);
+            foreach (var invokeItem in lastInvoked)
+                invokeItems.Remove(invokeItem);
+        }
+
+        public void Invoke(object instance, string methodName, float time = 0f)
+        {
+            invokeItems.Add(new InvokeItem
+            {
+                Instance = instance,
+                MethodName = methodName,
+                Time = currentTime + time,
+            });
+        }
+
+        public void InvokeRepeating(object instance, string methodName, float time, float repeatRate = -1)
+        {
+            invokeItems.Add(new InvokeItem
+            {
+                Instance = instance,
+                MethodName = methodName,
+                Time = currentTime + time,
+                LoopTime = repeatRate,
+            });
+        }
+
+        public bool IsInvoking(object instance, string methodName)
+        {
+            foreach (var invokeItem in invokeItems)
+                if (invokeItem.Instance == instance && invokeItem.MethodName == methodName)
+                    return true;
+            return false;
+        }
+
+        public TypeInvoker GetTypeInvoker(CrossBindingAdaptorType agent)
+        {
+            var type = agent?.ILInstance?.Type;
+            if (type == null)
+                return null;
+            TypeInvoker invoker;
+            if (typeInvokerMap.TryGetValue(type, out invoker))
+                return invoker;
+            invoker = new TypeInvoker(type);
+            typeInvokerMap.Add(type, invoker);
+            return invoker;
+        }
+    }
+}
