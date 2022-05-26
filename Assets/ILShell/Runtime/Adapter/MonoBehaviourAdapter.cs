888a54b8cd330dcdaa021891658d55ee584c402e
wynn <yowangyuxi@outlook.com>
2022-05-26 13:16:38 +0800

    MonoBehaviour Adapter

---
+++ Assets/ILShell/Runtime/Adapter/MonoBehaviourAdapter.cs
@@ -0,0 +1,20 @@
+﻿using ILRuntime.Runtime.Enviorment;
+using ILRuntime.Runtime.Intepreter;
+using ILShell.Agent;
+using System;
+
+namespace ILShell.Adapter
+{
+    public class MonoBehaviourAdapter : CrossBindingAdaptor
+    {
+        public override Type BaseCLRType => typeof(UnityEngine.MonoBehaviour);
+
+        public override Type AdaptorType => typeof(MonoBehaviourAgent);
+
+        public override object CreateCLRInstance(ILRuntime.Runtime.Enviorment.AppDomain appdomain, ILTypeInstance instance)
+        {
+            // ! you cannot new a MonoBehaviourAgent for it, because call MonoBehaviour's Constuctor is not allowed in Unity
+            return null;
+        }
+    }
+}
---
+++ Assets/ILShell/Runtime/Agent/MonoBehaviourAgent.cs
@@ -0,0 +1,68 @@
+﻿using ILRuntime.CLR.TypeSystem;
+using ILRuntime.Runtime.Enviorment;
+using ILRuntime.Runtime.Intepreter;
+using ILShell.Model;
+using ILShell.Runtime;
+using UnityEngine;
+
+namespace ILShell.Agent
+{
+    public class MonoBehaviourAgent : MonoBehaviour, CrossBindingAdaptorType
+    {
+        private ILTypeInstance instance;
+        private ILInvoker.TypeInvoker invoker;
+
+        public string ILType;
+
+        public ILData ILData;
+
+        public ILTypeInstance ILInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    if (this.ILType == null)
+                        return null;
+                    var type = ILAPP.GetType(this.ILType) as ILType;
+                    if (type != null)
+                    {
+                        instance = type.Instantiate();
+                        instance.CLRInstance = this;
+                        //MonoBehaviourAgentUtil.ILDeserialize(ILInstance, ILData);
+                        invoker = ILAPP.Current.ILInvoker.GetTypeInvoker(this);
+                    }
+                    else
+                        Debug.LogError($"[MonoBehaviourAgent]ILType not found: {ILType}");
+                }
+                return instance;
+            }
+
+            set
+            {
+                instance = value;
+            }
+        }
+
+        public void Awake()
+        {
+            ILInstance = ILInstance;
+            invoker?.Invoke(this, "Awake");
+        }
+
+        public void Start()
+        {
+            invoker?.Invoke(this, "Start");
+        }
+
+        public void OnEnable()
+        {
+            invoker?.Invoke(this, "OnEnable");
+        }
+
+        public void OnDisable()
+        {
+            invoker?.Invoke(this, "OnDisable");
+        }
+    }
+}
